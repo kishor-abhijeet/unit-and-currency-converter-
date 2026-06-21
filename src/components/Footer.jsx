fd71a65305f9203f41a92ad1ea48a1bc12468728
Abhijeet Kishor <abhijeetkishor24@gmail.com>
2026-06-21 17:09:21 +0530

    added new feature

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,7 @@ import { Mail } from "lucide-react";
 function Footer() {
   return (
     <footer className="mt-6 border-t border-gray-300 pt-4 text-center text-sm text-gray-600 ">
+
       <p>© {new Date().getFullYear()} Abhijeet Kishor</p>
 
       <div className="mt-2 flex items-center justify-center gap-2">
@@ -15,7 +16,16 @@ function Footer() {
         </a>
       </div>
 
-      
+      <div className="mt-5">
+        <a
+        href="https://digitalheroesco.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="inline-flex items-center rounded-lg bg-blue-600 px-5 py-2.5 text-sm font-semibold text-white transition hover:bg-blue-700">
+          Built for Digital Heroes
+        </a>
+      </div>
+
     </footer>
   );
 }
